d72772391430e549bad84bf5dc002ef315fd13e5
KevinGu <cagebooker@gmail.com>
2024-01-31 17:38:40 +0800

    [dev] 表格

--- src/pages/TradeRecord.jsx
+++ src/pages/TradeRecord.jsx
@@ -1,10 +1,39 @@
-import { useSearchParams } from "react-router-dom"
+import React, {useState} from 'react';
+// import { useSearchParams } from "react-router-dom"; 
+import dayjs from 'dayjs';
+import { DatePicker, Space, Form, Input, InputNumber, Popconfirm, Table, Typography } from 'antd';
+const { RangePicker } = DatePicker;
+const originData = [];
+for (let i = 0; i < 100; i++) {
+  originData.push({
+    index: `${i+1}`,
+    stockNum: '2330',
+    key: i.toString(),
+    buyTime: '12/31 09:56',
+    sellTime: '12-31 10:11',
+    buyPrice: '142',
+    sellPrice: '156', 
+    amount: '1000',
+    cost: '142,000',
+    strategy: '追高',
+    stopLoss: 139,
+    outcome: '勝',
+    profit: '2,000',
+    name: `Edward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`,
+  });
+}
+console.log(originData)
 
 const TradeRecord = () => {
-  const [searchParams] = useSearchParams();
-  console.log(searchParams.get("test"))
+  // const [searchParams] = useSearchParams();
   //@ ============================    變數    ============================
 	// React, Redux && 第三方
+  const [form] = Form.useForm();
+  const [data, setData] = useState(originData);
+  const [editingKey, setEditingKey] = useState('');
+  const isEditing = (record) => record.key === editingKey;
 	// 表單
 	// Button && FilterBar status
 	// 畫面資料
@@ -12,16 +41,247 @@ const TradeRecord = () => {
 	// loadingFlag
 	// 正則表達式
 
+  const columns = [
+    {
+      title: '編號',
+      dataIndex: 'index',
+      width: '6%',
+      editable: false,
+    },
+    {
+      title: '股號',
+      dataIndex: 'stockNum',
+      width: '5%',
+      editable: true,
+    },
+    {
+      title: '買進時間',
+      dataIndex: 'buyTime',
+      width: '11%',
+      editable: true,
+    },
+    {
+      title: '賣出時間',
+      dataIndex: 'sellTime',
+      width: '11%',
+      editable: true,
+    },
+    {
+      title: '買進價格',
+      dataIndex: 'buyPrice',
+      width: '9%',
+      editable: true,
+    },
+    {
+      title: '賣出價格',
+      dataIndex: 'sellPrice',
+      width: '7%',
+      editable: true,
+    },
+    {
+      title: '股數',
+      dataIndex: 'amount',
+      width: '8%',
+      editable: true,
+    },
+    {
+      title: '支出成本',
+      dataIndex: 'cost',
+      width: '10%',
+      editable: true,
+    },
+    {
+      title: '策略',
+      dataIndex: 'strategy',
+      width: '7%',
+      editable: true,
+    },
+    {
+      title: '停損價',
+      dataIndex: 'stopLoss',
+      width: '7%',
+      editable: true,
+    },
+    {
+      title: '勝敗',
+      dataIndex: 'outcome',
+      width: '6%',
+      editable: true,
+    },
+    {
+      title: '損益',
+      dataIndex: 'profit',
+      width: '8%',
+      editable: true,
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      width:'10%',
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable 
+        ? (<span>
+            <Typography.Link onClick={() => save(record.key)} style={{marginRight: 0,}}>
+              儲存
+            </Typography.Link><br />
+            <Popconfirm title="確定取消？" onConfirm={cancel}>
+              <a>取消</a>
+            </Popconfirm>
+          </span>) 
+        : (<Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+            編輯
+          </Typography.Link>
+        );
+      },
+    },
+  ];
 	//@ ============================ Functions ============================
-	
+	const edit = (record) => {
+    form.setFieldsValue({
+      name: '',
+      age: '',
+      address: '',
+      ...record,
+    });
+    setEditingKey(record.key);
+  };
+  const cancel = () => {
+    setEditingKey('');
+  };
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setData(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
 	//@ ============================    api.   ============================
 
 	//@ ============================ useEffect ============================
 	
 	//@ ============================   子頁面   ============================
-  const recordList = () => {
+  //! 不知道幹嘛的
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.dataIndex === 'age' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+  //& 編輯時的
+  const EditableCell = ({ editing, dataIndex, title, inputType, record, index, children, ...restProps}) => {
+    const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
     return (
-      <div className="recordList">123</div>
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item
+            name={dataIndex}
+            style={{
+              margin: 0,
+            }}
+            rules={[
+              {
+                required: true,
+                message: `Please Input ${title}!`,
+              },
+            ]}
+          >
+            {inputNode}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+  const recordList = () => {
+    const onRangeChange = (dates, dateStrings) => {
+      if (dates) {
+        console.log('From: ', dates[0], ', to: ', dates[1]);
+        console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
+      } else {
+        console.log('Clear');
+      }
+    };
+    const rangePresets = [
+      {
+        label: '過去 7 天',
+        value: [dayjs().add(-7, 'd'), dayjs()],
+      },
+      {
+        label: '過去 14 天',
+        value: [dayjs().add(-14, 'd'), dayjs()],
+      },
+      {
+        label: '過去 30 天',
+        value: [dayjs().add(-30, 'd'), dayjs()],
+      },
+      {
+        label: '過去 90 天',
+        value: [dayjs().add(-90, 'd'), dayjs()],
+      },
+    ];
+    return (
+      <div className="recordList">
+        <div className="recordList_title">交易紀錄</div>
+        <div className="recordList_filterBar">
+          <div className="recordList_filterBar-items">
+            <div className="recordList_filterBar-item">
+              <div className="recordList_filterBar-item-title">股號</div>
+              <input className="input" type="text" placeholder="EX. 2330"/>
+            </div>
+            <div className="recordList_filterBar-item">
+              <div className="recordList_filterBar-item-title">日期</div>
+              <Space direction="vertical" size={12}>
+                <RangePicker presets={rangePresets} onChange={onRangeChange} style={{ borderColor: '#C6C6C6'}}/>
+              </Space>
+            </div>
+          </div>
+          <button className="recordList_filterBar-btn">輸出</button>
+        </div>
+        <div className="recordList_list">
+        <Form form={form} component={false}>
+          <Table
+            components={{
+              body: {
+                cell: EditableCell,
+              },
+            }}
+            bordered
+            dataSource={data}
+            columns={mergedColumns}
+            rowClassName="editable-row"
+            pagination={{
+              onChange: cancel,
+            }}
+          />
+        </Form>
+        </div>
+      </div>
     )
   }
   const userInfo = () => {
